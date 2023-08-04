79955ccc7dba9885458b6e7cf4ceab04af1f4013
AdityaBhadange <aditya.bhadange18@gmail.com>
2023-08-04 22:05:39 +0530

    Initially Intigrated API's with LandingPage component

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { useGetRocketsQuery, useLazyGetCapsulesQuery } from '../redux/api'
+import MaterialReactTable, { MRT_ColumnDef } from 'material-react-table';
+import { useGetCapsules, useGetRockets } from '../redux/api.hooks';
 
 const LandingPage = () => {
-  return <h1>Landing Page</h1>
+  const rocketsQuery = useGetRockets();
+  const capsulesQuery = useGetCapsules();
 
-}
+  if (rocketsQuery.isLoadingGetRockets || capsulesQuery.isLoadingGetCapsules) {
+    return <div>Loading...</div>;
+  }
 
-export default LandingPage
+  if (rocketsQuery.isErrorGetRockets || capsulesQuery.isErrorGetCapsules) {
+    return <div>Error occurred while fetching data</div>;
+  }
+
+  const rockets = rocketsQuery.getRocketsData;
+  const capsules = capsulesQuery.getCapsulesData;
+
+  // Define table columns for rockets
+  const rocketColumns: MRT_ColumnDef[] = [
+    {
+      accessorKey: 'rocket_name',
+      header: 'Name'
+    }
+    // Add more columns based on the data structure
+  ];
+
+  // Define table columns for capsules
+  const capsuleColumns: MRT_ColumnDef[] = [
+    {
+      accessorKey: 'capsule_serial',
+      header: 'Name'
+    }
+    // Add more columns based on the data structure
+  ];
+
+  return (
+    <div>
+      <h2>Rockets</h2>
+      <MaterialReactTable columns={rocketColumns} data={rockets} />
+
+      <h2>Capsules</h2>
+      <MaterialReactTable columns={capsuleColumns} data={capsules} />
+    </div>
+  );
+};
+
+export default LandingPage;
